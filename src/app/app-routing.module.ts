191eaa00bc885cac5dada09050799057e244af55
OscarRAmirez <oerm16@gmail.com>
2020-06-02 06:59:02 -0500

    pruebas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PruebasComponent } from './componentes/componentes/pruebas/pruebas.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: 'home', component: PruebasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebasComponent } from './componentes/componentes/pruebas/pruebas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule,
